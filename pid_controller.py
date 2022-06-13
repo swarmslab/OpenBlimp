04ced304cb9af474990192a4e2c6e558c63546dd
Jiawei Xu <cyandicnx@hotmail.com>
2022-06-13 19:14:38 -0400

    Update pid_controller.py

--- pid_controller.py
+++ pid_controller.py
@@ -3,7 +3,7 @@ import time
 class PID:
     """PID controller."""
 
-    def __init__(self, Kp, Ki, Kd, origin_time=None):
+    def __init__(self, Kp, Ki, Kd, origin_time=None, circular=False, limit=0.0):
         if origin_time is None:
             origin_time = time.time()
 
@@ -20,16 +20,31 @@ class PID:
         self.previous_time = origin_time
         self.previous_error = 0.0
 
+        # accounts for euler angles
+        self.circular = circular
+        self.circular = limit
+
     def Update(self, error, current_time=None):
         if current_time is None:
             current_time = time.time()
         dt = current_time - self.previous_time
         if dt <= 0.0:
             return 0
+
         de = error - self.previous_error
+        if self.circular:
+            if de > self.circular:
+                de -= 2*self.circular
+            elif de < -self.circular:
+                de += 2*self.circular
 
         self.Cp = error
         self.Ci += error * dt
+        if self.Ci > 10:
+            self.Ci > 10
+        elif self.Ci <-10:
+            self.Ci =-10
+
         self.Cd = de / dt
 
         self.previous_time = current_time
